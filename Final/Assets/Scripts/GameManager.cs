37c29fdd0548f3e1fb02e61538a2cb6c831b28b7
Mamadou Diallo <mamadou.diallo13@mail.citytech.cuny.edu>
2020-12-13 02:47:11 -0500

    Bacteria
    Updated Bacteria for more variation in movement . 
    Created game manager script and made it so a set number of bacteria will be spawned over a set period of time. This avoids cluttering of enemies at the beginning. Bacteria are spawned both inside and outside the mouth simultaneously.
    
    Note: create panel at start of game that tells player to "Press R to switch to Inside/Outside of mouth

---
+++ Final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bc;
+    public GameObject bcs;
+    public GameObject bs;
+    public GameObject bsq;
+    public GameObject gc;
+    public GameObject gcs;
+    public GameObject gs;
+    public GameObject gsq;
+    public GameObject pc;
+    public GameObject pcs;
+    public GameObject ps;
+    public GameObject psq;
+    GameObject[] bacteria;
+
+    int hidden = 50;
+    int shown = 0;
+    void Start()
+    {
+        bacteria = new GameObject[] { bc, bcs, bs, bsq, gc, gcs, gs, gsq, pc, pcs, ps, psq };
+        InvokeRepeating("make", 3, 15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void make()
+    {
+        float x;
+        float y;
+        float z;
+        int random;
+        if (hidden > 0)
+        {
+            for (int i = 0; i < hidden; i++)
+            {
+                if (i >= 10)
+                {
+                    break;
+                }
+                x = Random.Range(-5, 5);
+                y = Random.Range((float)-1.5, 4);
+                bool front = Random.value > 0.5f;
+                if (front)
+                {
+                    z = (float)-3.78;
+                }
+                else
+                {
+                    z = -17;
+                }
+                random = Random.Range(0, 11);
+                hidden -= 1;
+                shown += 1;
+                Instantiate(bacteria[random], new Vector3(x, y, z), Quaternion.identity);
+            }
+        }
+
+    }
+}
--- Final/Assets/Scripts/bacteria.cs
+++ Final/Assets/Scripts/bacteria.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bacteria : MonoBehaviour
-{ 
+{
     float min = -5;
     float max = (float)5.75;
     float x;
-    float y; 
+    float y;
     float z;
     float angle = 0;
     bool increasing;
     public Transform myTransform;
-
+    bool wave;
     public float health;
     // Start is called before the first frame update
     void Start()
@@ -21,31 +21,65 @@ public class bacteria : MonoBehaviour
         y = this.transform.position.y;
         z = this.transform.position.z;
         increasing = true;
+        wave = Random.value > 0.5f;
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         angle += 2;
-        if(increasing)
+        if (wave)
         {
-            x += 0.02f;
-            y += Mathf.Sin((float)0.01*angle)/50;
-            myTransform.position = new Vector3(x, y, z);
-            if(x >= max)
+            if (increasing)
             {
-                increasing = !increasing;
+                x += 0.01f;
+                y += (Mathf.Sin((float)0.01 * angle) / 60);
+                myTransform.position = new Vector3(x, y, z);
+                if (x >= max)
+                {
+                    increasing = !increasing;
+                }
+            }
+            else
+            {
+                x -= 0.02f;
+                y += Mathf.Sin((float)0.01 * angle) / 50;
+                if (y > 5)
+                {
+                    y = 5;
+                }
+                myTransform.position = new Vector3(x, y, z);
+                if (x <= min)
+                {
+                    increasing = !increasing;
+                }
             }
         }
         else
         {
-            x -= 0.02f;
-            y += Mathf.Sin((float)0.01*angle)/50;
-            myTransform.position = new Vector3(x, y, z);
-            if(x <= min)
+            if (increasing)
             {
-                increasing = !increasing;
+                x += 0.01f;
+                myTransform.position = new Vector3(x, y, z);
+                if (x >= max)
+                {
+                    increasing = !increasing;
+                }
+            }
+            else
+            {
+                x -= 0.02f;
+                if (y > 5)
+                {
+                    y = 5;
+                }
+                myTransform.position = new Vector3(x, y, z);
+                if (x <= min)
+                {
+                    increasing = !increasing;
+                }
             }
         }
+
     }
 }
